549c211de581a8e5fcaee3a812e55e246ea886bf
ashikurrafi <ashikurrafi@gmail.com>
2022-02-13 01:42:27 +0600

    Bug C6385 fixed

--- Add numbers in array using pointer/Add numbers in array using pointer.cpp
+++ Add numbers in array using pointer/Add numbers in array using pointer.cpp
@@ -9,10 +9,10 @@ int main()
 	cout << "Enter size of array : ";
 	cin >> size;
 	array = new int[size];
-	for (int i = 0; i <= size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		cout << "Enter element " << i << " : ";
-		cin >> array[i];
+		cin>> array[i];
 	}
 	pointer = array;
 	for (int i = 0; i <= size; i++)
